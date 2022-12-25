938fc232344fb69ebae8f3c9f7d84a78058954e3
Alexandr Naum <mfq07k9cz@gmail.com>
2022-12-25 21:14:16 +0300

    test

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -61,7 +61,7 @@ export const getOne = async (req, res) => {
 
         res.json(doc)
       }
-    )
+    ).populate('user')
   } catch (err) {
     console.log(err)
     res.status(500).json({
@@ -110,7 +110,7 @@ export const create = async (req, res) => {
     const doc = new PostModel({
       title: req.body.title,
       text: req.body.text,
-      tags: req.body.tags,
+      tags: req.body.tags.split(','),
       imageUrl: req.body.imageUrl,
       user: req.userId
     })
@@ -137,7 +137,7 @@ export const update = async (req, res) => {
       {
         title: req.body.title,
         text: req.body.text,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
         imageUrl: req.body.imageUrl,
         user: req.userId
       }
